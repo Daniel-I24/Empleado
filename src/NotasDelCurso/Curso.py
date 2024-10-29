a1e0f78307dd159e8518863e5879e79384293474
Daniel-I24 <ivan242005d@gmail.com>
2024-10-29 09:46:04 -0500

    Add files via upload

---
+++ src/NotasDelCurso/Curso.py
@@ -0,0 +1,23 @@
+__author__="Daniel Igua"
+__licence__="GPL"
+__version__="1.0.0"
+__email__="ivan.igua@campusucc.edu.co"
+
+class Curso:
+
+    """# ----------------------------------------
+    # Constructor
+    -----------------------------------------#"""
+
+    def __init__(notas = int):
+        notas = []
+
+    def CalcularCantidadAprobados(self):
+        aprobados = 0
+        indice = 0
+
+        while indice < len(notas):
+            notas[indice] >= 3.0
+            indice += 1
+
+            return aprobados
---
+++ src/NotasDelCurso/Estudiante.py
@@ -0,0 +1,7 @@
+__author__="Daniel Igua"
+__licence__="GPL"
+__version__="1.0.0"
+__email__="ivan.igua@campusucc.edu.co"
+
+class Estudiante:
+    
